ca0abd9aa70a0cf4ebbdb9d8728ab80e4c026c52
David Song <david990724@gmail.com>
2022-01-30 21:12:20 -0500

    Update on algo-5

--- Algo-5/main.py
+++ Algo-5/main.py
@@ -1,5 +1,63 @@
+#BST
+
+class Node:
+    def __init__(self,val):
+        self.value = val
+        self.left = None
+        self.right = None
+    def _insert(self,data):
+        if data == self.value: #if key = self.value, failed to insert
+            return False
+        elif self.value > data: #if self.value is larger than data, go left to insert
+            if self.left: #if self.left exists, recursively call _insert for self.left
+                return self.left._insert(data)
+            else: #if self.left doesn't exist, add node at self.left and return True
+                self.left = Node(data)
+                return True
+        else: #if self.value is smaller than data, go right to insert
+            if self.right: #if self.right exists, recursively call _insert for self.right
+                return self.right._insert(data)
+            else: #if self.right doesn't exist, add node at self.right and return True
+                self.right = Node(data)
+                return True
+
+class Tree:
+    def __init__(self, root = None):
+        self.root = root
+    
+    def insert(self,data):
+        if self.root: #if root != None
+            return self.root._insert(data)
+        else: #if root == None
+            self.root = Node(data)
+            return True
+    
+    def search(self,data):
+        ()
+    
+    def delete(self,data):
+        ()
+    
+    def getMax(self):
+        ()
+
+    def getMin(self):
+        ()
+    
+    def inOrder(self):
+        ()
+    
+    def preOrder(self):
+        ()
+    
+    def postOrder(self):
+        ()
+
 def main():
-    ()
+    tr = Tree()
+    tr.insert(3)
+    tr.insert(5)
+    tr.insert(2)
 
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -48,7 +48,7 @@ Revising algorithms and data structures course concepts (ECE345).
 
 **_algo-4_**: probability and Bayes' theorem
 
-algo-5: binary search tree **[planned]** [need details]
+algo-5: binary search tree **[WIP]**
 
 algo-6: red-black tree **[planned]** [need details]
 
